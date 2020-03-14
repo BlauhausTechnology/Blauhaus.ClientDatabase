9676fa431b9522bb02bfe11a012d5874a360a8cf
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-14 11:29:12 +0200

    no message

---
+++ src/Blauhaus.ClientDatabase.Sqlite/_Ioc/IocServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Blauhaus.ClientDatabase.Sqlite.Config;
+using Blauhaus.ClientDatabase.Sqlite.Service;
+using Blauhaus.Ioc.Abstractions;
+
+namespace Blauhaus.ClientDatabase.Sqlite._Ioc
+{
+    public static class IocRegistration
+    {
+        public static IIocService RegisterLiteDb<TConfig>(this IIocService iocService) where TConfig : class, ISqliteConfig
+        {
+            iocService.RegisterImplementation<ISqliteConfig, TConfig>();
+            return Register(iocService);
+        }
+
+        private static IIocService Register(IIocService iocService)
+        {
+            iocService.RegisterImplementation<ISqliteDatabaseService, SqliteDatabaseService>();
+            return iocService;
+        }
+    }
+}
---
+++ src/Blauhaus.ClientDatabase.Sqlite/_Ioc/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Blauhaus.ClientDatabase.Sqlite.Config;
+using Blauhaus.ClientDatabase.Sqlite.Service;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.ClientDatabase.Sqlite._Ioc
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterLiteDb<TConfig>(this IServiceCollection services) where TConfig : class, ISqliteConfig
+        {
+            services.AddScoped<ISqliteConfig, TConfig>();
+            return Register(services);
+        }
+
+        private static IServiceCollection Register(IServiceCollection services)
+        {
+            services.AddScoped<ISqliteDatabaseService, SqliteDatabaseService>();
+            return services;
+        }
+    }
+}
