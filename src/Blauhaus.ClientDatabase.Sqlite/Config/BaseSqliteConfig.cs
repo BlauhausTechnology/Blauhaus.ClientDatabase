f413b699bd8262d52d4dd5e9a48e38fd8066f6ec
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-08-19 17:46:25 +0200

    no message

---
+++ src/Blauhaus.ClientDatabase.Sqlite/Config/BaseSqliteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Blauhaus.DeviceServices.Abstractions.DeviceInfo;
+
+namespace Blauhaus.ClientDatabase.Sqlite.Config
+{
+    public abstract class BaseSqliteConfig : ISqliteConfig
+    {
+        protected BaseSqliteConfig(IDeviceInfoService deviceInfoService, string databaseName)
+        {
+
+            if (!databaseName.EndsWith(".sqlite"))
+            {
+                databaseName += ".sqlite";
+            }
+
+            DatabasePath = Path.Combine(deviceInfoService.AppDataFolder, databaseName);
+            
+            TableTypes = new List<Type>
+            { 
+
+                //Vessels
+
+            };
+        }
+         
+
+        public string DatabasePath { get; protected set; }
+        public IList<Type> TableTypes { get; protected set; }
+
+
+    }
+}
